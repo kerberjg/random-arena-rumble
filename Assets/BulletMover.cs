87113f402173ba43bfbe253fdd0a985f6b342c7b
max <max.hemberg@gmail.com>
2020-02-05 12:05:07 +0100

    Added GunShooting

---
+++ Assets/BulletMover.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMover : MonoBehaviour
+    
+{
+    public float bulletSpeed = 5f;
+
+    void Update()
+    {
+        transform.Translate(0, bulletSpeed * Time.deltaTime, 0);
+    }
+}
