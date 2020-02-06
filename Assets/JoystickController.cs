35fac019325b6969d1d62189a80593ec2b6120e3
sini tang <Sini.Tang.9176@student.uu.se>
2020-02-06 19:20:44 +0100

    added joystick

---
+++ Assets/JoystickController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoystickController : MonoBehaviour
+{
+    public bool isMovingDown = false;
+    public bool pressed = false;
+
+    public float DownSpeed = 10f;
+    public float UpSpeed = 5f;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && pressed == false)
+        {
+            pressed = true;
+            isMovingDown = true;
+        }
+        if(pressed == true)
+        {
+            if(isMovingDown == true)
+            {
+                transform.eulerAngles -= new Vector3(DownSpeed, 0, 0) * Time.deltaTime;
+            }
+            else if(isMovingDown == false)
+            {
+                transform.eulerAngles += new Vector3(UpSpeed, 0, 0) * Time.deltaTime;
+            }
+            if (transform.eulerAngles.x - 360 < -85)
+            {
+                isMovingDown = false;
+            }
+            if(transform.eulerAngles.x - 360 < -355 && pressed == true && isMovingDown == false)
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                pressed = false;
+            }
+        }
+    }
+}
