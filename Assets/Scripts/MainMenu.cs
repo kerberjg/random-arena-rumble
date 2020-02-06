d58b9a7990b8895f460904031773b4da4de36ad8
Liam Forsberg <fourzifer@gmail.com>
2020-02-06 12:14:38 +0100

    Start/Quit functionality, TMP implementation

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Player");
+    }
+
+    public void QuitGame ()
+    {
+        Debug.Log("RIP RAR, died too soon");
+        Application.Quit();
+    }
+}
