84490a1457e69fed9a22a3d0dfdba337825a6f34
max <max.hemberg@gmail.com>
2020-02-07 17:05:13 +0100

    Implements ModifierManager and Weapon Swapping

---
+++ Assets/ModifierManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModifierManager : MonoBehaviour
+{
+    ModifierWeapons modifierWeapons;
+    // Update is called once per frame
+
+    private void Start()
+    {
+        modifierWeapons = GameObject.Find("Player").GetComponent<ModifierWeapons>();
+    }
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Alpha1)) {
+            modifierWeapons.weaponType = 1;
+        } else if(Input.GetKey(KeyCode.Alpha2)){
+            modifierWeapons.weaponType = 2;
+        } else if (Input.GetKey(KeyCode.Alpha3)) {
+            modifierWeapons.weaponType = 3;
+        }
+    }
+}
