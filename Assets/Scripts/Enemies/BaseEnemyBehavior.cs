e049479d9a364d2ab4bf784f24b25810d4a926ab
James Kerber <kerber.jg@gmail.com>
2020-02-07 02:14:37 +0100

    Fixed BaseEnemyBehavior

--- Assets/Scripts/Enemies/BaseEnemyBehavior.cs
+++ Assets/Scripts/Enemies/BaseEnemyBehavior.cs
@@ -8,24 +8,29 @@ enum ComparisonResult {
 abstract public class BaseEnemyBehavior : MonoBehaviour
 {
     [Header("Tracking options")]
+    public bool enableTracking = true;
     public Transform target;
     public float updateFrequency = 1f;
-    public float lerpSpeed = 1f;
-    protected Vector3 targetPos { get; private set; }
+    public Vector3 targetPos { get; private set; }
     
     [Header("Following options")]
+    public bool enableFollow = true;
     public float minTargetDistance = 0f;
     public float maxTargetDistance = 0f;
     public float distanceTolerance = 0.1f;
     public float followSpeed = 0f;
 
-    private Vector3 _currentPos;
-    private Vector3 _prevPos;
+    [Header("Movement debug")]
+    [SerializeField]
+    private Vector3 _currentPos = new Vector3();
+    [SerializeField]
+    private Vector3 _prevPos = new Vector3();
+    [SerializeField]
     private float _lastUpdate;
 
     void UpdateTracking() {
         float now = Time.time;
-        float currentStep = _lastUpdate - now;
+        float currentStep = now - _lastUpdate;
         float stepLength = 1f / updateFrequency;
 
         // update tracker position
@@ -34,55 +39,74 @@ abstract public class BaseEnemyBehavior : MonoBehaviour
             _currentPos = target.position;
             _lastUpdate = now;
         }
-
         // lerp position over time
-        if(lerpSpeed > 0) {
+        else {
             targetPos = Vector3.Lerp(_prevPos, _currentPos, currentStep / stepLength);
-        } else {
-            targetPos = _currentPos;
         }
     }
 
+    [SerializeField]
     private Vector3 _tmpDist = new Vector3();
+
     void UpdateFollow() {
         // calculate distance to target
         _tmpDist = this.transform.position - targetPos;
         float distance = _tmpDist.magnitude;
+        _tmpDist.Normalize();
 
         // if following is activated, move towards/away from the target...
         if(followSpeed > 0f) {
-            // ignore if within tolerance
-            if(Mathf.Abs(distance) >= distanceTolerance) {
-                return;
-            }
-
             // move towards the target
             if(
-                (maxTargetDistance > 0 && distance > maxTargetDistance)
+                (maxTargetDistance > 0 && CompareDistances(distance, maxTargetDistance, distanceTolerance) > 0)
                 ||
-                (minTargetDistance > 0 && distance > minTargetDistance)
+                (minTargetDistance > 0 && CompareDistances(distance, minTargetDistance, distanceTolerance) > 0)
             ) {
-                this.transform.position += _tmpDist.Normalize() * followSpeed * Time.deltaTime;
+                _tmpDist = this.transform.position - _tmpDist * followSpeed * Time.deltaTime;
+                Debug.Log("too far, follow");
             }
-
             // move away from the target
-
+            else if(minTargetDistance > 0 && CompareDistances(distance, minTargetDistance, distanceTolerance) < 0) {
+                _tmpDist = this.transform.position + _tmpDist * followSpeed * Time.deltaTime;
+                Debug.Log("too close, follow");
+            } else {
+                Debug.Log("within constraints, follow");
+                return;
+            }
         }
         // ...otherwise just stay within the distance range
         else {
-            if(maxTargetDistance > 0 && distance > maxTargetDistance) {
-                this.transform.position = targetPos + _tmpDist.Normalize() * maxTargetDistance;
-            } else if(minTargetDistance > 0 && distance < minTargetDistance) {
-                this.transform.position = targetPos + _tmpDist.Normalize() * minTargetDistance;
+            if(maxTargetDistance > 0 && CompareDistances(distance, maxTargetDistance, distanceTolerance) > 0) {
+                _tmpDist = targetPos + _tmpDist * maxTargetDistance;
+                Debug.Log("over max, no follow");
+            } else if(minTargetDistance > 0 && CompareDistances(distance, minTargetDistance, distanceTolerance) < 0) {
+                _tmpDist = targetPos + _tmpDist * minTargetDistance;
+                Debug.Log("over max, no follow");
+            } else {
+                Debug.Log("within constraints, no follow");
+                return;
             }
         }
+
+        this.transform.position = _tmpDist;
     }
 
     void Update() {
-        UpdateTracking();
-        UpdateFollow();
+        if(enableTracking) UpdateTracking();
+        if(enableFollow) UpdateFollow();
 
         EnemyUpdate();
     }
     abstract public void EnemyUpdate();
+
+    private float CompareDistances(float d, float constraint, float tolerance) {
+        float val = d - constraint;
+
+        if(Mathf.Abs(val) <= tolerance)
+            return 0;
+        else if(val > 0)
+            return +1;
+        else
+            return -1;
+    }
 }
