c476459306bd3d0aa21f7f798179ed824ef9616b
Liam Forsberg <fourzifer@gmail.com>
2020-02-07 17:08:05 +0100

    Updated title, implemented game over scene

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -12,7 +12,29 @@ public class MainMenu : MonoBehaviour
 
     public void QuitGame ()
     {
-        Debug.Log("RIP RAR, died too soon");
+        Debug.Log("Game is closed");
         Application.Quit();
+    }
+
+    bool gameHasEnded = false;
+
+    public void GameOver ()
+    {
+        if (gameHasEnded == false)
+        {
+            gameHasEnded = true;
+            Debug.Log("Goes to Game Over screen");
+            Invoke("toGameOverScreen", 2f);
+        }
+    }
+
+    public void toMainMenuScene()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    void toGameOverScene()
+    {
+        SceneManager.LoadScene("GameOver");
     }
 }
