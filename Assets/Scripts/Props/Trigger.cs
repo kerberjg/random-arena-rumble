593fe6447baa9ec2699c04190bf6fbafcfcf66ea
James Kerber <kerber.jg@gmail.com>
2020-02-28 16:46:11 +0100

    Implemented Trigger and TriggerController scripts

---
+++ Assets/Scripts/Props/Trigger.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+    public TriggerController controller;
+    public int value = 1;
+
+    public Material materialUnactive;
+    public Material materialActive;
+    bool active = false;
+    
+    void OnEnable() {
+        controller.Register(this);
+    }
+
+    void OnTriggerCollision2D(Collider2D collider) {
+        if(!active && collider.gameObject.tag == "Player") {
+            Activate();
+        }
+    }
+
+    void LateUpdate() {
+        if(this.active && controller.currentCount == 0) {
+            Deactivate();
+        }
+    }
+
+    public void Activate() {
+        active = true;
+
+        Renderer r;
+        if(TryGetComponent<Renderer>(out r)) {
+            r.material = materialActive;
+        }
+
+        SoundManager.i.PlayOnce("ButtonActivate");
+    }
+
+    public void Deactivate() {
+        active = false;
+
+        Renderer r;
+        if(TryGetComponent<Renderer>(out r)) {
+            r.material = materialActive;
+        }
+
+        SoundManager.i.PlayOnce("ButtonDeactivate", true);
+    }
+}
