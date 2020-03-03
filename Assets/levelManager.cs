5c3a3d2a9eef438f593e809a75047022e43c50bc
max <max.hemberg@gmail.com>
2020-03-03 01:50:54 +0100

    Implemented Level 3 + Prepared for build

---
+++ Assets/levelManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class levelManager : MonoBehaviour
+{
+
+    public void loadLevelOne()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
