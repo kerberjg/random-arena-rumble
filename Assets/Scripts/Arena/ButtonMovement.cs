e8cdc0a5506b681eca81e9c1680a482562d1a921
Stefan <sovergaard96@gmail.com>
2020-03-06 17:46:35 +0100

    Level3 Zone 1 - 4 In sniperLevel folder

--- Assets/Scripts/Arena/ButtonMovement.cs
+++ Assets/Scripts/Arena/ButtonMovement.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 public class ButtonMovement : MonoBehaviour
 {
     public float delta = 1f;
-    public float moveSpeed = 1f;
+    public float maxSpeed = 3f;
+    public float minSpeed = 1f;
 
     bool moveUp = true;
     private float topValue;
     private float botValue;
+    private float moveSpeed;
 
     private void Start()
     {
         topValue = transform.position.y + delta;
         botValue = transform.position.y - delta;
+
+        moveSpeed = Random.Range(minSpeed, maxSpeed);
     }
     private void Update()
     {
