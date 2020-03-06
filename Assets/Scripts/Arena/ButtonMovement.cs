de96ccd3a6175ab703e1f914cd964d8c12d150c1
Stefan <sovergaard96@gmail.com>
2020-03-06 17:19:37 +0100

    Script and Scene changes

--- Assets/Scripts/Arena/ButtonMovement.cs
+++ Assets/Scripts/Arena/ButtonMovement.cs
@@ -4,12 +4,32 @@ using UnityEngine;
 
 public class ButtonMovement : MonoBehaviour
 {
-    public float speed = 1f;
-    public float delta = 3f;
+    public float delta = 1f;
+    public float moveSpeed = 1f;
 
+    bool moveUp = true;
+    private float topValue;
+    private float botValue;
+
+    private void Start()
+    {
+        topValue = transform.position.y + delta;
+        botValue = transform.position.y - delta;
+    }
     private void Update()
     {
-        float y = Mathf.PingPong(speed * Time.time, (transform.position.y + delta));
-        transform.position = new Vector3(transform.position.x, y, transform.position.z);
+        if (transform.position.y > topValue) 
+        {
+            moveUp = false;
+        }
+        else if (transform.position.y < botValue)
+        {
+            moveUp = true;
+        }
+        if (moveUp)
+            transform.position = new Vector3(transform.position.x, transform.position.y + moveSpeed * Time.deltaTime, transform.position.z);
+        else
+            transform.position = new Vector3(transform.position.x, transform.position.y - moveSpeed * Time.deltaTime, transform.position.z);
     }
+
 }
